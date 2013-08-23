805bf33529ff78ca4c5e082ad1351acb6ca0629b
Scott Thomson <scotty3785@gmail.com>
2013-08-23 16:20:45 +0100

    First trials with bottle

---
+++ Bottle/bottle_test.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+#  untitled.py
+#  
+#  Copyright 2012 Scott Thomson <scotty3785@gmail.com>
+#  
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
+#  
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#  
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+#  MA 02110-1301, USA.
+#  
+#  
+from bottle import route, run, template
+
+@route('/hello/<name>')
+def index(name='World'):
+    return template('<b>Hello {{name}}</b>!', name=name)
+    
+@route('/hello/')
+def index():
+    return '<html><title>Bottle Test</title><b>Hello World</b>'
+
+run(host='localhost', port=8080,reloader=True)
